2a26d6efa67de1dd8efa674ebd676a3f906620e7
许林 <xulin.wh@qq.com>
2018-12-17 17:41:54 +0800

    棋子绘制

--- GobangOc/GobangOc/GobangView.m
+++ GobangOc/GobangOc/GobangView.m
@@ -40,4 +40,60 @@ static NSInteger kBoardSize = 13;
     
     return self;
 }
+
+// 监听手势信息
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"touchesBegan withEvent");
+    
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self];
+    
+    NSUInteger h, v;
+    for (int i = 0; i <= kBoardSize; i++) {
+        if (i * self.frame.size.width / (kBoardSize + 1) <= point.x && point.x < (i + 1) * self.frame.size.width / (kBoardSize + 1)) {
+            if (i == 0) {
+                h = 0;
+                return;
+            }
+            
+            if (i == kBoardSize) {
+                h = kBoardSize - 1;
+                return;
+            }
+            
+            if (fabs(i * self.frame.size.width / (kBoardSize + 1) - point.x) >= fabs((i + 1) * self.frame.size.width / (kBoardSize + 1) - point.x)) {
+                h = i;
+                break;
+            } else {
+                h = i - 1;
+                break;
+            }
+        }
+            
+        if (i * self.frame.size.width / (kBoardSize + 1) <= point.x && point.y < (i + 1) * self.frame.size.width / (kBoardSize + 1)) {
+            if (i == 0) {
+                v = 0;
+                return;
+            }
+            
+            if (i == kBoardSize) {
+                v = kBoardSize - 1;
+                return;
+            }
+            
+            if (fabs(i * self.frame.size.width / (kBoardSize + 1) - point.y) >= fabs((i + 1) * self.frame.size.width / (kBoardSize + 1) - point.y)) {
+                v = i;
+                break;
+            } else {
+                v = i - 1;
+                break;
+            }
+        }
+    }
+    
+    UIImageView *piece = [[UIImageView alloc] initWithFrame:(CGRectMake(0,0,7.5,7.5))];
+    piece.image = [UIImage imageNamed:@"black"];
+    [self addSubview:piece];
+    piece.center = CGPointMake((h + 1) * self.frame.size.width / (kBoardSize + 1), (v + 1) * self.frame.size.width / (kBoardSize + 1));
+}
 @end
